6daefa712a43590e7d28fb7587b4fe52113cbd18
Darek Sikorra <dariusz.sikorra@gmail.com>
2019-10-31 21:34:18 +0100

    Updates

---
+++ precache-manifest.b530c74eaa8edd326fa84099b89eb309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91d96ba69c3589b2041ddcecd870ab3",
+    "url": "/TodoList-Typescript/index.html"
+  },
+  {
+    "revision": "6a8885dd730004d7501f",
+    "url": "/TodoList-Typescript/static/css/main.bdc8f1df.chunk.css"
+  },
+  {
+    "revision": "6a0cfe538cba7b78fcac",
+    "url": "/TodoList-Typescript/static/js/2.606fc94f.chunk.js"
+  },
+  {
+    "revision": "6a8885dd730004d7501f",
+    "url": "/TodoList-Typescript/static/js/main.144a57fd.chunk.js"
+  },
+  {
+    "revision": "34847364f57f7e77c574",
+    "url": "/TodoList-Typescript/static/js/runtime-main.5d3076d9.js"
+  }
+]);
